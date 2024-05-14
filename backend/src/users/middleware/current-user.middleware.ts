2570564d08c9583e2e27eedbe84a1e5db067cbe3
Deepanshu Garg <95443111+deepanshug1@users.noreply.github.com>
2024-05-14 13:00:08 +0530

    Update current-user.middleware.ts

--- backend/src/users/middleware/current-user.middleware.ts
+++ backend/src/users/middleware/current-user.middleware.ts
@@ -4,27 +4,14 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
-export class currentUserMiddleware implements NestMiddleware {
+export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    // Handle OPTIONS requests
-    if (req.method === 'OPTIONS') {
-      // Set CORS headers
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PUT, DELETE, OPTIONS',
-      );
-      res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Authorization',
-      );
-      res.setHeader('Access-Control-Max-Age', '86400'); // 24 hours
-
-      // Respond to OPTIONS request with 200 status
-      res.sendStatus(200);
-    } else {
-      // Continue to the next middleware or route handler
-      next();
-    }
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization',
+    );
+    next();
   }
 }
